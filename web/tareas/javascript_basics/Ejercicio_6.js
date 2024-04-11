f1a352cccf3c3e53ef0a0944840752195000348c
Lisette <lismelor@gmail.com>
2024-04-11 17:45:26 -0600

    Ejercicio_6 hecho

---
+++ web/tareas/javascript_basics/Ejercicio_6.js
@@ -0,0 +1,9 @@
+function hackerSpeak(cadena) {
+    // Mapeo de letras y sus equivalentes en hacker speak
+    const hackerMap = {
+        'a': '4',
+        'e': '3',
+        'i': '1',
+        'o': '0',
+        's': '5'
+    };
